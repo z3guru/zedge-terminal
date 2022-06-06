c48b19afe6e0bc607987ed0f8559584af98fc626
zcube <zeze@z3.guru>
2022-06-06 10:43:27 +0900

    setup dev-env on other machine

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const net = require('net')
 
-let serverSocket = net.createServer(function(client) {
-	client.on();
+let client = net.connect({port:1112, host:'127.0.0.1'}, function(client) {
+	// connected...
 	
+});
 
-	
+client.on('data', function(data) {
 
+});
 
-}
+client.on('end', function(data) {
+
+});
+
 
